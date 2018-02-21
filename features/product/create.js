2b65c0bad2b9b374dc13b8daa89a5c9510a423f9
Nathan Bland <nathan.bland@gmail.com>
2018-02-21 14:14:17 -0700

    apply db fix to create.js as well

--- features/product/create.js
+++ features/product/create.js
@@ -1,8 +1,10 @@
 'use strict';
-const db = require('../../db.js')
-const Product = require('../../models/product')
+const Mongoose = require('mongoose')
+Mongoose.Promise = global.Promise
 
 module.exports.createProduct = (event, context, callback) => {
+  const mongoose = Mongoose.createConnection('mongodb://admin:admin@ds243728.mlab.com:43728/items-db')
+  const Product = require('../../models/product')(mongoose)
   const body = JSON.parse(event.body);
 
   const product = new Product({
@@ -12,19 +14,21 @@ module.exports.createProduct = (event, context, callback) => {
     stock: body.stock,
     imageURL: body.imageURL
   })
-  product.save()
-  .then(product => {
-    Product.db.close()
-    return callback(null, {
-      statusCode: 201,
-      body: JSON.stringify({
-        product: product
+  Product.db.once('open', () => {
+    product.save()
+    .then(product => {
+      Product.db.close()
+      return callback(null, {
+        statusCode: 201,
+        body: JSON.stringify({
+          product: product
+        })
       })
     })
-  })
-  .catch(err => {
-    Product.db.close()
-    return callback(new Error(err))
+    .catch(err => {
+      Product.db.close()
+      return callback(new Error(err))
+    })
   })
 
   // callback(null, response);
