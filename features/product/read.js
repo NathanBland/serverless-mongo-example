ddd4c20cfe2291be82fc2b24570f7fa9ebc1dbe9
Nathan Bland <nathan.bland@gmail.com>
2018-02-21 13:54:14 -0700

    for now, exclude db.js. Fix model override error in mongoose.

--- features/product/read.js
+++ features/product/read.js
@@ -1,27 +1,35 @@
 'use strict';
-const db = require('../../db.js')
-const Product = require('../../models/product')
+// const db = require('../../db.js')
+const Mongoose = require('mongoose')
+Mongoose.Promise = global.Promise
 
 module.exports.readProduct = (event, context, callback) => {
-  let productId = ''
+  const mongoose = Mongoose.createConnection('mongodb://admin:admin@ds243728.mlab.com:43728/items-db')
+  const Product = require('../../models/product')(mongoose)
+  
+  let productId = ""
   let query = {}
   if (event.pathParameters) {
     query = {_id: event.pathParameters.id}
   }
-  Product.find(query)
-  .then(product => {
-    Product.db.close()
-    return callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({
-        product: product
+  console.time('query')
+  Product.db.once('open', () => {
+    Product.find(query)
+    .then(product => {
+      Product.db.close()
+      console.timeEnd('query')
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          product: product
+        })
       })
     })
+    .catch(err => {
+      Product.db.close()
+      return callback(new Error(err))
+    })
   })
-  .catch(err => {
-    Product.db.close()
-    return callback(new Error(err))
-  })
-
+    
   // callback(null, response);
 };
--- models/product.js
+++ models/product.js
@@ -1,19 +1,20 @@
 const uuid = require('uuid')
 const mongoose = require('mongoose')
 
-const product =  mongoose.model('product',{
-  _id: {
-    type: String,
-    default: uuid.v4()
-  },
-  name: String,
-  itemCode: String,
-  price: Number,
-  stock: {
-    type: Number,
-    default: 1
-  },
-  imageURL: String
-})
-
-module.exports = product
+module.exports = (db) => {
+  return db.model('product', {
+    _id: {
+      type: String,
+      default: uuid.v4()
+    },
+    name: String,
+    itemCode: String,
+    price: Number,
+    stock: {
+      type: Number,
+      default: 1
+    },
+    imageURL: String
+  })
+}
+//  = product
